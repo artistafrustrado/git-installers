5e69ded9fe3ce1708bf5cabf55dd8a5be66f88d1
Fernando Michelotti <artista.frustrado@gmail.com>
2022-06-25 15:34:08 -0300

    added blender support

---
+++ blender.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+mkdir /tmp/blender-git
+cd /tmp/blender-git
+git clone https://git.blender.org/blender.git 1
+cd blender
+git submodule update --init --recursive
+git submodule foreach git checkout master
+git submodule foreach git pull --rebase origin master
+make update
+cd /tmp/blender-git
+#./blender/build_files/build_environment/install_deps.sh
+
