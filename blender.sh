7486f889dd80c208a2a1e722c646cac82c2595f0
Fernando Michelotti <artista.frustrado@gmail.com>
2022-06-26 17:19:44 -0300

    updated blender scripi acording to documentation

--- blender.sh
+++ blender.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 
+#mkdir /tmp/blender-git
+#cd /tmp/blender-git
+#git clone https://git.blender.org/blender.git 1
+#cd blender
+#git submodule update --init --recursive
+#git submodule foreach git checkout master
+#git submodule foreach git pull --rebase origin master
+#make update
+#cd /tmp/blender-git
+#./blender/build_files/build_environment/install_deps.sh
+
+cd /tmp
 mkdir /tmp/blender-git
-cd /tmp/blender-git
-git clone https://git.blender.org/blender.git 1
+cd blender-git
+git clone git://git.blender.org/blender.git
 cd blender
 git submodule update --init --recursive
 git submodule foreach git checkout master
 git submodule foreach git pull --rebase origin master
-make update
-cd /tmp/blender-git
-#./blender/build_files/build_environment/install_deps.sh
 
+make && make install
